c4211a5ea2a6d1051c34c5c831163fd97eac9a26
Bruno Canepa <bru.canepa@gmail.com>
2017-09-20 20:40:23 -0300

    Modified function names & Modified actions to be an Object instead of an Array

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
-function isFunction (obj) {
-  return !!(obj && obj.constructor && obj.call && obj.apply)
-}
 
-function createActionHandler (ignore) {
-  // redux-ignore higher order reducer
-  return function handleAction (reducer, actions = []) {
-    const predicate = isFunction(actions)
-        ? actions
-        : (action) => actions.indexOf(action.type) >= 0
+const createActionHandler = ignore => {
+  return (reducer, actions = {}) => {
+    const predicate = (action) => actions[action.type];
 
-    const initialState = reducer(undefined, {})
+    const initialState = reducer(undefined, {});
 
     return (state = initialState, action) => {
       if (predicate(action)) {
         return ignore ? state : reducer(state, action)
       }
+      return ignore ? reducer(state, action) : state;
+    };
+  };
+};
 
-      return ignore ? reducer(state, action) : state
-    }
-  }
-}
+export const createReducerByFilteringActions = createActionHandler(false);
 
-export const ignoreActions = createActionHandler(true)
-export const filterActions = createActionHandler(false)
-
-export default ignoreActions
-// /redux-ignore
+export const createReducerByIgnoringActions = createActionHandler(true);
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 import assert from 'assert'
 import { createStore } from 'redux'
 
-import { ignoreActions, filterActions } from '../src/index'
+import { createReducerByIgnoringActions, createReducerByFilteringActions } from '../src/index'
 
 let reducer = (state, action) => {
   switch (action.type) {
@@ -14,9 +14,9 @@ let reducer = (state, action) => {
   }
 }
 
-describe('ignoreActions()', () => {
+describe('createReducerByIgnoringActions()', () => {
   it('should ignore actions with specified types in array', () => {
-    let ignoringReducer = ignoreActions(reducer, ['BAR'])
+    let ignoringReducer = createReducerByIgnoringActions(reducer, {'BAR': true})
     let action = { type: 'BAR' }
 
     assert.equal(
@@ -25,7 +25,7 @@ describe('ignoreActions()', () => {
   })
 
   it('should not ignore actions that do not have types in array', () => {
-    let ignoringReducer = ignoreActions(reducer, ['BAR'])
+    let ignoringReducer = createReducerByIgnoringActions(reducer, {'BAR': true})
     let action = { type: 'FOO' }
 
     assert.equal(
@@ -34,27 +34,18 @@ describe('ignoreActions()', () => {
   })
 
   it('should allow all actions when no action types array is specified', () => {
-    let ignoringReducer = ignoreActions(reducer)
+    let ignoringReducer = createReducerByIgnoringActions(reducer)
     let action = { type: 'BAZ' }
 
     assert.equal(
       ignoringReducer('testing', action),
       'default-state')
   })
-
-  it('should work with a predicate function for actions', () => {
-    let ignoringReducer = ignoreActions(reducer, (a) => a.invalid)
-    let action = { type: 'BAR', invalid: true }
-
-    assert.equal(
-      ignoringReducer('testing', action),
-      'testing')
-  })
 })
 
-describe('filterActions()', () => {
+describe('createReducerByFilteringActions()', () => {
   it('should include actions with specified types in array', () => {
-    let filteringReducer = filterActions(reducer, ['BAR'])
+    let filteringReducer = createReducerByFilteringActions(reducer, {'BAR': true})
     let action = { type: 'BAR' }
 
     assert.equal(
@@ -63,7 +54,7 @@ describe('filterActions()', () => {
   })
 
   it('should exclude actions that do not have types in array', () => {
-    let filteringReducer = filterActions(reducer, ['BAR'])
+    let filteringReducer = createReducerByFilteringActions(reducer, {'BAR': true})
     let action = { type: 'FOO' }
 
     assert.equal(
@@ -72,7 +63,7 @@ describe('filterActions()', () => {
   })
 
   it('should exclude all actions when no action types array is specified', () => {
-    let filteringReducer = filterActions(reducer)
+    let filteringReducer = createReducerByFilteringActions(reducer)
     let action = { type: 'BAZ' }
 
     assert.equal(
@@ -80,17 +71,8 @@ describe('filterActions()', () => {
       'testing')
   })
 
-  it('should work with a predicate function for actions', () => {
-    let filteringReducer = filterActions(reducer, (a) => a.valid)
-    let action = { type: 'BAR', valid: true }
-
-    assert.equal(
-      filteringReducer('testing', action),
-      'bar-state')
-  })
-
   it('should return an initial state when a redux store is created', () => {
-    let filteringReducer = filterActions(reducer, ['BAR'])
+    let filteringReducer = createReducerByFilteringActions(reducer, {'BAR': true})
     let store = createStore(filteringReducer)
 
     assert.equal(
